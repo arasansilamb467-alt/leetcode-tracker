9553bb1dbba2a5b49ac5a0acf9965ede36501de2
arasansilamb467-alt <arasansilamb467@gmail.com>
2026-07-09 10:14:46 +0530

    Create 4332-CheckGoodInteger

---
+++ 4332-CheckGoodInteger/4332-CheckGoodInteger.java
@@ -0,0 +1,14 @@
+// Last updated: 7/9/2026, 10:14:45 AM
+class Solution {
+    public boolean checkGoodInteger(int n) {
+        int digitSum=0;
+        int squareSum=0;
+        while (n>0){
+            int digit=n%10;
+            digitSum+=digit;
+            squareSum+=digit*digit;
+            n/=10;
+        }
+        return (squareSum-digitSum)>=50;
+    }
+}
